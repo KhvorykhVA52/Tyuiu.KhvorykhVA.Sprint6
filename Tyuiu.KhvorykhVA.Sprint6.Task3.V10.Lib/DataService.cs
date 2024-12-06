3ca64a940a5c155de8bbf3caff8d70cca2e6a192
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-12-07 00:33:20 +0500

    Task3

--- Tyuiu.KhvorykhVA.Sprint6.Task3.V10.Lib/DataService.cs
+++ Tyuiu.KhvorykhVA.Sprint6.Task3.V10.Lib/DataService.cs
@@ -1,26 +1,25 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.KhvorykhVA.Sprint6.Task3.V10.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task3V10
     {
-        public int Calculate(int[,] array)
+        public int[,] Calculate(int[,] matrix)
         {
-            int count = 0;
-            int rows = array.GetLength(0);
-            int cols = array.GetLength(1);
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
             if (rows >= 5)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    if (array[4, j] % 2 == 0)
+                    if (matrix[4, j] % 2 == 0)
                     {
-                        count++;
+                        matrix[4, j] = 0;
                     }
                 }
             }
 
-            return count;
+            return matrix;
         }
     }
 }
